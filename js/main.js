f161d1993ed95655cdf8d9ab486bb03486684670
kadonomaro <denis.afer@gmail.com>
2019-02-07 03:39:49 +0700

    optimize images, added small images for tablet screen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 window.addEventListener('load', function () {
-    
+    let bubbleBG = document.querySelector('.header__background-bubble');
+    window.addEventListener('scroll', function () {
+        console.log(window.pageYOffset);
+        
+    })
 });
