bb34879caade211bd9a0f5aa85df218c326d4669
kadonomaro <denis.afer@gmail.com>
2019-02-11 23:09:59 +0700

    added parallax images, remove inner slider item

--- js/parallax.js
+++ js/parallax.js
@@ -1,11 +1,13 @@
 export function parallax (backgroundArr) {
     
     let backgrounds = document.querySelectorAll(backgroundArr);
-    document.addEventListener('mousemove', function (evt) {
+    backgrounds[0].addEventListener('mousemove', function (evt) {
         let coordX = (evt.clientX / window.innerWidth * 100 - 50).toFixed(0);
         for (let i = 0; i < backgrounds.length; i++) {
             backgrounds[i].style = `transform: translateX(${coordX / backgrounds[i].dataset.divider}%)`;
         }
-    })
+    });
+
+
 };
 
