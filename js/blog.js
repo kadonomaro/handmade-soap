6632127a2f919dad2dc30bcd580da3ac0876793c
kadonomaro <denis.afer@gmail.com>
2019-06-23 18:15:36 +0700

    added dropdown menu

--- js/blog.js
+++ js/blog.js
@@ -3,5 +3,18 @@ import scrollTop from "./modules/scrollTop.js";
 document.addEventListener('DOMContentLoaded', function () {
 
     scrollTop(document.querySelector('.scroll-top'), 'scroll-top--active');
+
+    let blogHeaderNav = document.querySelector('.blog-header__nav');
+    let blogHeaderNavOffset = blogHeaderNav.offsetTop;
+
+    
+    window.addEventListener('scroll', function (evt) {
+
+        if (window.pageYOffset >= blogHeaderNavOffset) {
+            blogHeaderNav.classList.add('blog-header__nav--fixed');
+        } else {
+            blogHeaderNav.classList.remove('blog-header__nav--fixed');
+        }
+    });
     
 });
