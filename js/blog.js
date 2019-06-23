d3c874f49240fe6747249603c754f7d7434f2abb
kadonomaro <denis.afer@gmail.com>
2019-06-23 22:39:31 +0700

    added mobile nav toggle

--- js/blog.js
+++ js/blog.js
@@ -4,11 +4,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
     scrollTop(document.querySelector('.scroll-top'), 'scroll-top--active');
 
+    let blogNavToggle = document.querySelector('.blog-nav__toggle');
+    let blogNavList = document.querySelector('.blog-nav__list');
+
+    blogNavToggle.addEventListener('click', function (evt) {
+
+        if (blogNavList.classList.contains('blog-nav__list--active')) {
+            blogNavList.classList.remove('blog-nav__list--active');
+            // blogNavList.addEventListener('transitionend', function () {
+                
+            // });
+            setTimeout(function () {
+                blogNavList.style.display = 'none';
+            },500);
+        } else {
+            blogNavList.style.display = 'block';
+            blogNavList.classList.add('blog-nav__list--active');
+
+        }
+        
+    });
+
+
     let blogHeaderNav = document.querySelector('.blog-header__nav');
     let blogHeaderNavOffset = blogHeaderNav.offsetTop;
-
-    
-    window.addEventListener('scroll', function (evt) {
+    window.addEventListener('scroll', function () {
 
         if (window.pageYOffset >= blogHeaderNavOffset) {
             blogHeaderNav.classList.add('blog-header__nav--fixed');
