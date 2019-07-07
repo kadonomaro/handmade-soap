24c699d9bee60bbc2d57a77295e5ca6210ea724d
kadonomaro <denis.afer@gmail.com>
2019-07-07 23:34:55 +0700

    changed tabs content visual, added kb event

--- js/modules/tabs.js
+++ js/modules/tabs.js
@@ -2,21 +2,31 @@ export default function tabs(tabs, content, tabActiveClass, contentActiveClass) 
     
     tabs.forEach(tab => {
         tab.addEventListener('click', function () {
-            tabs.forEach(tab => {
-                tab.classList.remove(tabActiveClass);
-            });
-            tab.classList.add(tabActiveClass);
-
-            content.forEach(targetContent => {
-                if (tab.dataset.index === targetContent.dataset.index) {
-                    content.forEach(targetContent => {
-                        targetContent.classList.remove(contentActiveClass);
-                    });
-                    targetContent.classList.add(contentActiveClass);
-                }
-            });
-
+            switchTab(tab);
         });
+        tab.addEventListener('keyup', function (evt) {
+            if (evt.which === 13) {
+                switchTab(tab);
+            }
+        });
+
+
     });
+
+    function switchTab(tab) {
+        tabs.forEach(tab => {
+            tab.classList.remove(tabActiveClass);
+        });
+        tab.classList.add(tabActiveClass);
+
+        content.forEach(targetContent => {
+            if (tab.dataset.index === targetContent.dataset.index) {
+                content.forEach(targetContent => {
+                    targetContent.classList.remove(contentActiveClass);
+                });
+                targetContent.classList.add(contentActiveClass);
+            }
+        });
+    }
 
 }
