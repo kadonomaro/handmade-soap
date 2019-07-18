eabb01e3758355c66841abaf5f62238d3d7d8311
kadonomaro <denis.afer@gmail.com>
2019-07-18 22:57:40 +0700

    added bubbles images to canvas

--- js/modules/floatingBubbles.js
+++ js/modules/floatingBubbles.js
@@ -5,14 +5,31 @@ export default function floatingBubbles() {
 
     let ctx = canvas.getContext('2d');
 
-    let bubble1 = new Image();
-    bubble1.src = '../images/bubbles/bubble-1.png';
+    let bubble = new Image();
+    bubble.src = '../images/bubbles/bubble-3.png';
 
-    bubble1.addEventListener('load', function () {
-        ctx.drawImage(bubble1, 130, 90);
-        ctx.drawImage(bubble1, 700, 80);
-        ctx.drawImage(bubble1, 600, 730);
-        ctx.drawImage(bubble1, 600, 730);
+    bubble.addEventListener('load', function () {
+        ctx.drawImage(bubble, 220,680, 60,60);
+        ctx.drawImage(bubble, 740,600, 60,60);
+
+        ctx.drawImage(bubble, 1400,45, 100,100);
+        ctx.drawImage(bubble, 1700,220, 100,100);
+        ctx.drawImage(bubble, 1300,570, 100,100);
+
+        ctx.drawImage(bubble, 130,90, 150,150);
+        ctx.drawImage(bubble, 700,80, 150,150);
+        ctx.drawImage(bubble, 600,730, 150,150);
+        ctx.drawImage(bubble, 1300, 800, 150,150);
+        ctx.drawImage(bubble, 300,280, 150,150);
+        
+        ctx.drawImage(bubble, 500,400, 225,225);
+        ctx.drawImage(bubble, 1175,180, 225,225);
+        ctx.drawImage(bubble, 1400,400, 225,225);
+    });
+
+    canvas.addEventListener('mousemove', function (evt) {
+        console.log('X: ', evt.x);
+        console.log('Y: ', evt.y);
     });
 
     
