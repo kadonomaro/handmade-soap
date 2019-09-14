3c890ea0f26ed00d371d5d7708ed5d167ad21196
kadonomaro <denis.afer@gmail.com>
2019-09-14 22:57:21 +0700

    feat: add anim for background block, add button for this

--- js/faq.js
+++ js/faq.js
@@ -4,10 +4,20 @@ import accordion from "./modules/accordion.js";
 
 document.addEventListener('DOMContentLoaded', function () {
 
+    const faq = document.querySelector('.faq');
     const accordionBlock = document.querySelector('.accordion');
 
     scrollTop(document.querySelector('.scroll-top'), 'scroll-top--active');
     navigation();
     accordion(accordionBlock);
 
+    const faqFadeButton = document.querySelector('.faq__button');
+    faqFadeButton.addEventListener('click', function () {
+        faq.classList.add('faq--animated');
+        this.style.opacity = 0;
+        setTimeout(() => {
+            accordionBlock.classList.remove('accordion--hidden');
+            this.style.visibility = 'hidden';
+        }, 300);
+    });
 });
