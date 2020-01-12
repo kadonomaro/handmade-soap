4ac4c57fb56548eabfbb925d552198f0e58a3c82
kadonomaro <denis.afer@gmail.com>
2020-01-12 18:08:10 +0700

    feat: add validation error (message, color)

--- js/modules/subscribe.js
+++ js/modules/subscribe.js
@@ -1,14 +1,24 @@
 import modal from "./modal.js";
 
 export default function subscribe() {
-    const subscribeButton = document.querySelector('.js-subscribe-button');
-    const subscribeEmail = document.querySelector('.js-subsribe-email');
+
+    const subscribeBlock = document.querySelector('.subscribe');
+    const subscribeButton = subscribeBlock.querySelector('.js-subscribe-button');
+    const subscribeEmail = subscribeBlock.querySelector('.js-subsribe-email');
+    const subscribeInvalidText = subscribeBlock.querySelector('.js-subscribe-invalid');
 
     subscribeButton.addEventListener('click', function (evt) {
         evt.preventDefault();
 
         if (subscribeEmail.validity.valid) {
             modal(this.dataset.target);
+        } else {
+            subscribeEmail.classList.add('subscribe__input--invalid');
+            subscribeInvalidText.textContent = subscribeEmail.validationMessage;
+            setTimeout(() => {
+                subscribeEmail.classList.remove('subscribe__input--invalid');
+                subscribeInvalidText.textContent = '';
+            }, 300000);
         }
     });
 }
