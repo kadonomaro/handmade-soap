728e47601b7a6b7f8b2eee02483e8fec78e19ec9
kadonomaro <denis.afer@gmail.com>
2020-01-12 20:59:28 +0700

    feat: body modal class, close button icon, some css change

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -5,14 +5,16 @@ export default function modal(id) {
 
     modalOverlay.classList.add('modal-overlay--active');
     modal.classList.add('modal--active');
-
+    document.body.classList.add('page--modal-open');
+    
+    modalClose.focus();
     modalOverlay.addEventListener('click', closeModal);
-
 
     function closeModal(evt) {
         if (evt.target === modalOverlay || evt.target === modalClose) {
             modalOverlay.classList.remove('modal-overlay--active');
             modal.classList.remove('modal--active');
+            document.body.classList.remove('page--modal-open');
             modalOverlay.removeEventListener('click', closeModal);
         }
     }
